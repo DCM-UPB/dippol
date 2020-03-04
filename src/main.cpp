18a4ac50a85a4598d27d280424998af7478dd004
Hossam Elgabarty <hossam.elgabarty@uni-paderborn.de>
2020-03-04 14:09:38 +0100

    partially switching to c++ in main, using loos to load molecular system

---
+++ src/main.cpp
@@ -0,0 +1,60 @@
+// The aim of this program is to simulate SFG spectra.
+// The dipole moment and the polarizability are calculated
+// thanks to first order approximation
+
+// BE CAREFUL
+// To write the code I supposed that:
+// 1)The position file is constituted of water only (dummy atoms for TIP4P can be included)
+// 2)The water model is not flexible (easy calculation of the bissector)
+// 3)The O and H are never splitted because of pbc
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <main.h>
+#include <treat_inp.h>
+#include <loos.hpp>
+//#include <boost/program_options.hpp>
+//#include <boost/filesystem.hpp>
+//#include <numeric>
+//#include <algorithm>
+#include <iomanip>
+//#include <exception>
+
+int main(int argc, char *argv[]){
+  input_info input={0,0,"","",""};
+  sys_info sys={{0.,0.,0.},0,0,0,0,0,0,0,0.0,				\
+		0,0,0,NULL,0,0,0,0,					\
+		0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,	\
+		0.0,0.0,0.0,0.0,					\
+		0.0,0.0,0.0,0.0,0.0,0.0,0.0};
+
+#ifdef DEBUG
+  cout << "You are running the DEBUG-mode.\n";
+  cout << "I hope you know what you are doing.\n";
+  cout << "If you want the release mode: change the makefile!\n\n";
+#endif
+
+  //The file where the convergence problems are reported is removed
+  remove(CONV_PB);
+
+  // Read the input file
+  read_input(&input,&sys,argc,argv);
+
+  //loos::AtomicGroup current_frame = loos::createSystem(input.namei);
+  //std::cout << current_frame.size() << std::endl;
+  
+  //current_frame.periodicBox(PBOX);
+  
+  //pTraj traj = createTrajectory(vm["xyz"].as<string>().c_str(),current_frame);
+  
+  
+  //  Open/Read/Close the position file
+  //  Each step, the polarizability and
+  //  the dipole moment are calculated
+  trajectory(&sys,input,argv);
+  
+  return 0;
+}
+
+
